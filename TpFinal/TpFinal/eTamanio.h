9b6e5f7d2aa65d2e50ffbd07eddd3b69ff7b6265
isadione <isadi@LAPTOP-A6R0FKV3>
2023-06-02 13:35:41 -0300

    enum

--- TpFinal/TpFinal/eTamanio.h
+++ TpFinal/TpFinal/eTamanio.h
@@ -1,1 +1,8 @@
 #pragma once
+
+#ifndef _ETAMANIO_H
+#define _ETAMANIO_H
+
+enum eTamanio { pequenio, mediano, grande };
+
+#endif
--- TpFinal/TpFinal/eUbicacion.h
+++ TpFinal/TpFinal/eUbicacion.h
@@ -1,1 +1,8 @@
 #pragma once
+
+#ifndef _EUBICACION_H
+#define _EUBICACION_H
+
+enum eUbicacion { cabeza, pulmon, cuello, mama, utero, ojo, tiroides, prostata, intestino };
+
+#endif
