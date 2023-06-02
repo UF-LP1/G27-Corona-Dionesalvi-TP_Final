435bef0aefec4b4365244269a959e5e8250678cb
catalinacorona <coronacatalina8@gmail.com>
2023-06-02 14:00:25 -0300

    ccentro_radioterapia .h .cpp

--- TpFinal/TpFinal/cCentro_radioterapia.cpp
+++ TpFinal/TpFinal/cCentro_radioterapia.cpp
@@ -1,1 +1,15 @@
 #include "cCentro_radioterapia.h"
+
+cCentro_radioterapia::cCentro_radioterapia(string direccion, list<cPaciente> pacientes, list<cEmpleado> empleados) {
+
+	this->direccion = direccion;
+	this->pacientes = &pacientes;
+	this->empleados = &empleados;
+}
+
+void contactar_paciente();
+
+void buscar_paciente_limite_radiacion();
+
+void buscar_paciente_en_tratamiento();
+
--- TpFinal/TpFinal/cCentro_radioterapia.h
+++ TpFinal/TpFinal/cCentro_radioterapia.h
@@ -1,5 +1,34 @@
 #pragma once
+#ifndef _CCENTRO_RADIOTERAPIA_H
+#define _CCENTRO_RADIOTERAPIA_H
+
+#include <list>
+#include <string>
+
+#include "cPaciente.h"
+#include "cEmpleado.h"
+
+
+using namespace std;
+
 class cCentro_radioterapia
+
 {
+public:
+	
+	cCentro_radioterapia(string direccion, list<cPaciente> pacientes,list <cEmpleado> empleados);
+	
+	void contactar_paciente();
+	void buscar_paciente_limite_radiacion();
+	void buscar_paciente_en_tratamiento();
+
+	~cCentro_radioterapia();
+
+private: 
+	string direccion;
+	list <cPaciente> *pacientes;
+	list <cEmpleado> *empleados;
+
 };
 
+#endif
